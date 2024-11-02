9498c9f0b5e16d008ca75eb4892fa5cff803a404
iamcap <599149391@qq.com>
2024-11-03 01:08:16 +0800

    chg pic

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    assetsDir: 'assets',
-  }
+    plugins: [react()],
+    build: {
+        assetsDir: 'assets',
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+    },
 })
